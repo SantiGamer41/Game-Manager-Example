450e2ce74aa8da10c3b49872f6ad2395f20008bc
SantiGamer41 <dalessanti41@gmail.com>
2024-08-16 10:39:49 -0300

    Trancision de escenas

---
+++ Assets/soundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public AudioSource SourceSFX;
+    public static soundManager Instance;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        SourceSFX.PlayOneShot(clip);
+    }
+}
